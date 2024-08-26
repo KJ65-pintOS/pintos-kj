b5dbfb66297f755d249126b8bde71c114d877aa2
hab56ur9 <hab5bur9@gmail.com>
2024-08-26 11:11:23 +0000

    초기 버전 저장용

--- threads/thread.c
+++ threads/thread.c
@@ -316,7 +316,7 @@ thread_yield (void) {
 
 	old_level = intr_disable ();
 	if (curr != idle_thread)
-		list_insert_ordered(&ready_list, &curr->elem, timer_less_func,NULL);
+		list_push_back(&ready_list, &(curr->elem));
 	do_schedule (THREAD_READY);
 	intr_set_level (old_level);
 }
