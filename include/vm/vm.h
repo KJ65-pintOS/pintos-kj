4b30ce1d90846a724d2b90e59ec4cc1d29472cfb
ll5215 <foxlover17@gmail.com>
2024-09-24 13:37:23 +0900

    test

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -85,6 +85,7 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
+	struct hash *spt_hash;
 };
 
 #include "threads/thread.h"
